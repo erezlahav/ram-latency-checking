872333969ec4860a920cf8f289c618ecb1b61ee6
Erez <erezlahav10@gmail.com>
2026-01-14 21:40:43 +0200

    adding caches latency checking

--- ram_latency_checking.c
+++ ram_latency_checking.c
@@ -6,9 +6,15 @@
 #include <pthread.h>
 #include <sched.h>
 #include <sys/mman.h>
+#include <string.h>
+
+#define RAM_CHECK_ARRAY_SIZE 4 *1024 * 1024 
+#define L1_CHECK_ARRAY_SIZE 16 * 1024
+#define L2_CHECK_ARRAY_SIZE 256 * 1024
+#define L3_CHECK_ARRAY_SIZE 2 * 1024 * 1024
 
 
-#define ARRAY_SIZE 1 *1024 * 1024 
+
 #define NUMBER_OF_CORES 16
 #define ITERATIONS_NUMBER 10000000
 
@@ -27,12 +33,12 @@ void pin_to_core(int core_number){
 
 
 
-void shuffle_indexes(uint32_t* arr){
-    for(int i = 0; i < ARRAY_SIZE;i++){
+void shuffle_indexes(uint32_t* arr,size_t array_size){
+    for(size_t i = 0; i < array_size;i++){
         arr[i] = i;
     }
-    for(int i = 1; i < ARRAY_SIZE;i++){
-        int temp_val = arr[i];
+    for(size_t i = 1; i < array_size;i++){
+        size_t temp_val = arr[i];
         long rand_index = rand() % (i);
         arr[i] = arr[rand_index];
         arr[rand_index] = temp_val;
@@ -42,20 +48,14 @@ void shuffle_indexes(uint32_t* arr){
 
 
 
-int main(){
-    pin_to_core(rand() % NUMBER_OF_CORES-1); //cpu affinity
-
-    struct sched_param p = { .sched_priority = 99 };
-    sched_setscheduler(0, SCHED_FIFO, &p);
-    //prevents from context switching in the middle of the calculation
-
-
-    uint32_t* arr = malloc(ARRAY_SIZE * sizeof(uint32_t));
+double get_ram_latency(){
+    size_t full_malloc_size = RAM_CHECK_ARRAY_SIZE * sizeof(uint32_t);
+    uint32_t* arr = malloc(full_malloc_size);
     if(arr == NULL){
-        printf("failed to allocate %d bytes\n",ARRAY_SIZE);
+        printf("failed to allocate %d bytes\n",RAM_CHECK_ARRAY_SIZE);
         return 0;
     }
-    shuffle_indexes(arr);
+    shuffle_indexes(arr,RAM_CHECK_ARRAY_SIZE);
 
     uint64_t start,end;
 
@@ -65,7 +65,75 @@ int main(){
         array_index = arr[array_index];
     }
     end = get_current_tsc();
-    printf("RAM latency (cycles per access): %f\n", (double)(end - start) / ITERATIONS_NUMBER);
+    free(arr);
+    return (double)(end - start) / ITERATIONS_NUMBER;
+}
+
+
+
+
+
+
+
+double get_cache_latency(int cache_number){
+
+    size_t size_of_array;
+    switch (cache_number){
+        case 1:
+            size_of_array = L1_CHECK_ARRAY_SIZE;
+            break;    
+        case 2:
+            size_of_array = L2_CHECK_ARRAY_SIZE;
+            break;
+        case 3:
+            size_of_array = L3_CHECK_ARRAY_SIZE;
+            break;
+        default:
+            size_of_array = -1;
+            break;
+    }
+    if(size_of_array == -1)
+        return -1.0;
+
+
+    size_t full_malloc_size = size_of_array * sizeof(uint32_t);
+    uint32_t* arr = malloc(full_malloc_size);
+    shuffle_indexes(arr,size_of_array);
+    volatile int tmp;
+    uint64_t start,end;
+    start = get_current_tsc();
+    for(int i = 0; i < size_of_array;i+=65){
+        tmp = arr[tmp];
+    }
+    end = get_current_tsc();
+    free(arr);
+    return (double)(end - start) / (size_of_array/65);
+}
+
+
+int main(int argc,char* argv[]){
+
+    if(argc != 2){
+        printf("usage : ./program l1/l2/l3/ram\n");
+        return 0;
+    } 
+
+
+    pin_to_core(rand() % NUMBER_OF_CORES-1); //cpu affinity
+
+    struct sched_param p = { .sched_priority = 99 };
+    sched_setscheduler(0, SCHED_FIFO, &p);
+    //prevents from context switching in the middle of the calculation
+
+
+
+    double latency_in_cycles;
+    if(strcmp(argv[1],"l1") == 0) latency_in_cycles = get_cache_latency(1); 
+    else if(strcmp(argv[1],"l2") == 0) latency_in_cycles = get_cache_latency(2);
+    else if(strcmp(argv[1],"l3") == 0) latency_in_cycles = get_cache_latency(3);
+    else if(strcmp(argv[1],"ram") == 0) latency_in_cycles = get_ram_latency();
+
+    printf("latency (cycles per access): %f\n", latency_in_cycles);
 }
 
 
